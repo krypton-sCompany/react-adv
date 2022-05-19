5ea0c2a87dc939225d08f31e153a28ec2eec18b0
Martin <milone@fktech.net>
2022-05-19 14:43:32 -0300

    fix routes

--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -17,19 +17,19 @@ const Lazy3 = lazy( () => import('../01-lazyload/pages/LazyPage3'))
 export const routes: Route[] = [
     {
         to: '/lazy1',
-        path: '/lazy1',
+        path: 'lazy1',
         Component: Lazy1,
         name: 'Lazy-1'
     },
     {
         to: '/lazy2',
-        path: '/lazy2',
+        path: 'lazy2',
         Component: Lazy2,
         name: 'Lazy-2'
     },
     {
         to: '/lazy3',
-        path: '/lazy3',
+        path: 'lazy3',
         Component: Lazy3,
         name: 'Lazy-3'
     }
